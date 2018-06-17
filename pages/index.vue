d6d3bbdbdc67f0a6896548462dc9c1146214210d
Your Name <ko.kiedtisak@gmail.com>
2018-06-17 10:59:19 +0700

    progra

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+<div class="">
+  <div><h1><center>HISTORY</center></h1>
+  <div><center><img src="pic/ko.jpg" hight="300" width="300"></center>
+  <div><h1><center>นายเกียรติศักดิ์ มะนาศรี</center></h1></div>
+  <div><h1><center>อายุ 20 ปี</center></h1></div>
+  <div><h1><center>ที่อยูู่ 96 หมู่ 21 ต.หนองตาด อ.เมือง จ.บุรีรัมย์</center></h1></div>
+  <div><h1><center>รหัสนักศึกษา 6142040601</center></h1></div>
+  </div>
+  </div>
+  </div>
+  </template>  
+  <script>
+  export default {
+    data() {
+      return {
+        id:1
+      }
+    }
+  }
+  </script>
+  
+  
+  
